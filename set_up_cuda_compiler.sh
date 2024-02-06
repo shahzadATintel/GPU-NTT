9d3fbc4d43d81319cceaa927d25412d7e3f4bdde
Shahzad Ahmad Butt <shahzad.ahmad.butt@intel.com>
2024-02-06 14:01:49 -0800

    fix some types

--- set_up_cuda_compiler.sh
+++ set_up_cuda_compiler.sh
@@ -1,2 +1,2 @@
-$ cmake . -D CMAKE_CUDA_ARCHITECTURES=86 -D MODULAR_REDUCTION_TYPE=0 -B./cmake-build 
-$ cmake --build ./cmake-build/ --target ntt_cpu_test --parallel
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
