d6346fcb8d4179c1a678f84b7f5f52c71cd2a0f2
Shahzad Ahmad Butt <shahzad.ahmad.butt@intel.com>
2024-02-06 14:03:07 -0800

    fix some types

--- build_ntt_cpu.sh
+++ build_ntt_cpu.sh
@@ -1,2 +1,2 @@
 cmake . -D CMAKE_CUDA_ARCHITECTURES=86 -D MODULAR_REDUCTION_TYPE=0 -B./cmake-build 
-cmake --build ./cmake-build/ --target ntt_cpu_test --parallel
+cmake --build ./cmake-build/ --target test_ntt_cpu --parallel
